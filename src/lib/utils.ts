46024d3b6fbac11bb44e8f83aa047452b044d734
kennethazin <kenneth.ras3@mail.dcu.ie>
2024-12-30 20:10:00 +0000

    remove unused getCurrentDate function from utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,14 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-
-export function getCurrentDate(separator=''){
-
-  let newDate = new Date()
-  let date = newDate.getDate();
-  let month = newDate.getMonth() + 1;
-  let year = newDate.getFullYear();
-  
-  return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
-  }
