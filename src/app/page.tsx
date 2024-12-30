3052309ad8be693e1938f189394bf8dc9d1e2fe3
kennethazin <kenneth.ras3@mail.dcu.ie>
2024-12-30 21:22:29 +0000

    style: adjust padding in main layout and update date display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-14">
-      <h1 className="text-2xl font-medium text-center mb-4">
+    <main className="flex min-h-screen flex-col items-center justify-center p-5 lg:p-14">
+      <h1 className="text-2xl font-medium text-center">
       {new Date().toLocaleDateString() + ""}      
       </h1>
       <TodoList />
